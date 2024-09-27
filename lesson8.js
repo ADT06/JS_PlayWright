27860b8df72421f1bbaa7916e599c5d41540847b
ADT06 <anu.tewari@ishidaeurope.com>
2024-09-27 15:11:04 +0100

    created functions: declaring functions, Anonymous function, ES6 function syntax  or arrow function, Function with arguments, function with return

--- lesson8.js
+++ lesson8.js
@@ -1,6 +1,34 @@
-//declaring functions
+//Declaring functions
+//can be called in the beginning of the file
+
 
 function helloOne(){
     console.log('Hello one!')
 }
-helloOne() //to see it displaye
+helloOne() //to see it Hello one displayed in terminal
+
+//Anomymuos function, this function does not have a name, so needs to be assigned to a variable
+var helloTwo = function(){
+    console.log('Hello two!')
+}
+helloTwo()
+
+//ES6 function syntax  or arrow function
+var helloThree = () =>{
+    console.log('Hello Three!')
+}
+helloThree()  
+
+//Function with arguments
+function printTheName(name, lastName){
+    console.log(name + ' '+ lastName) // code is not dependent on the hard coded data
+}
+printTheName('John', 'Pop')
+
+//function with return
+function multiplyByThree (number){
+    var result = number * 3
+    return result
+}
+var myresult = multiplyByThree(9)
+console.log(myresult)
