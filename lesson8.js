a6b12b73a28abfd317c430c57c5aeb8a0aa29a39
ADT06 <anu.tewari@ishidaeurope.com>
2024-09-30 08:49:26 +0100

    lesson9 created

--- lesson8.js
+++ lesson8.js
@@ -19,14 +19,15 @@ var helloThree = () =>{
 }
 helloThree()  
 
-//Function with arguments, The values that you use inside of the parentheses , name and lastname,  of the function.
-//Call the parameters of the function and 
+//Function with arguments, 
+//The values that you use inside of the parentheses , name and lastname,  of the function.
+//Called the parameters of the functions  
 
 function printTheName(name,lastName){
 console.log(name + ' '+ lastName) // code is not dependent on the hard coded data
 }
 printTheName('John', 'Pop')
-//the values that you pass into , like John and Pop , are the functions are the arguments of the functions 
+//the values that you pass into , like John and Pop , are the functions, are the arguments of the functions 
 //that are passed into the function and used inside of the function.
 
 //function with return
@@ -36,14 +37,14 @@ function multiplyByThree (number){
 }
 var myresult = multiplyByThree(9)
 console.log(myresult)
+//here it is assigned to the variable
 
 //importing function
-import {printAge} from '../JS_PlayWright/helpers/printHelper.js' //importing from printerHelper.js file
-printAge(5) //is called from this file lesson 8
+import {printAge} from '../JS_PlayWright/helpers/printHelper.js' //importing from printerHelper.js file export function has been used there
+printAge(5) //is called the function to use it
 
 //import everything, form an other 
 import * as helper from '../JS_PlayWright/helpers/printHelper.js' 
 helper.printAge (10)
-//you can import all the functions from the JavaScript using import start as provide the name of
-//the object 'helper', how you want to use this function, and then through the dot notation you call the function
-//inside of the file.
+//you can import all the functions from the JavaScript using import start as provide the name of the object 'helper', 
+//how you want to use this function, and then through the dot.notation you call the function inside of the file.
