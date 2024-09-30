a5ce728713949ffe710f9ea88aea27a9c3ea041a
ADT06 <anu.tewari@ishidaeurope.com>
2024-09-30 11:28:32 +0100

    fixed error terminal command

--- helpers/printHelper.js
+++ helpers/printHelper.js
@@ -1,19 +1,3 @@
-
-//exporting funtion linked to lesson 8 - importing function - outside this file
-export function printAge(age){ 
-    console.log(age)
-}
-printAge()
-
-//And you can import the function into the different Java classes. For that, you need to 
-//export this function first with the keyword export and then you call import name
-// of the function and provide the relative path to the function location.
-//And then you simply call this function and use it.
-// Also, you can import all the functions from the JavaScript using import start as provide the name of the object, 
-//how you want to use this function, and then through the dot notation you call the function inside of the file.
-
-//create inside the the class the methods
-// Exporting functions
 export function add(a, b) {
     return a + b;
 }
@@ -21,9 +5,7 @@ export function add(a, b) {
 export function subtract(a, b) {
     return a - b;
 }
- //class gets created by the keyword and use capitals in the beginning of the word
- //if you export the class it will be visible in other files in JS 
-// Exporting a class as default
+ 
 export default class Person {
     constructor(name, age) {
         this.name = name;
@@ -34,3 +16,10 @@ export default class Person {
         console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`);
     }
 }
+
+//how you want to use this function, and then through the dot notation you call the function inside of the file.
+//create inside the the class the methods
+// Exporting functions
+//class gets created by the keyword and use capitals in the beginning of the word
+// if you export the class it will be visible in other files in JS 
+// Exporting a class as default
--- lesson9.js
+++ lesson9.js
@@ -1,16 +1,20 @@
-//Class and Methods
+import { add, subtract } from './helpers/printHelper.js';
+import Person from './helpers/printHelper.js';
 
-//type import and name and vscode will give a hint which class is available to choose and by clicking enter, vs code will create the import
+
+console.log(add(2, 3));                     
+console.log(subtract(5, 2));               
+
+
+const tanya = new Person('Tanya', 25);        
+tanya.greet();       
+
+
+
+//Output: 5 // Using the imported functions
+// Output: 3
+// type import and name and vscode will give a hint which class is available to choose and by clicking enter, vs code will create the import
+// Using the imported class
+// Output: Hello, my name is John and I am 25 years old.
 
 // Importing functions and class
-import { add, subtract } from './';
-import Person from './person.js';
-
-// Using the imported functions
-console.log(add(2, 3)); // Output: 5
-console.log(subtract(5, 2)); // Output: 3
-
-// Using the imported class
-const john = new Person('John', 25);
-john.greet(); // Output: Hello, my name is John and I am 25 years old.
-
