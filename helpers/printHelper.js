bec6fb2d21696ae8400cbb7e2673fadc8cb82068
ADT06 <anu.tewari@ishidaeurope.com>
2024-09-30 10:43:42 +0100

    fixing error command line

--- helpers/printHelper.js
+++ helpers/printHelper.js
@@ -13,10 +13,24 @@ printAge()
 //how you want to use this function, and then through the dot notation you call the function inside of the file.
 
 //create inside the the class the methods
-export class CustomerDetails {
-    printFirstName(name) {
-        console.log(name);
-    }
+// Exporting functions
+export function add(a, b) {
+    return a + b;
+}
+
+export function subtract(a, b) {
+    return a - b;
 }
  //class gets created by the keyword and use capitals in the beginning of the word
  //if you export the class it will be visible in other files in JS 
+// Exporting a class as default
+export default class Person {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    greet() {
+        console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`);
+    }
+}
--- lesson9.js
+++ lesson9.js
@@ -1,11 +1,17 @@
 //Class and Methods
 
 //type import and name and vscode will give a hint which class is available to choose and by clicking enter, vs code will create the import
-import {CustomerDetails} from "./"; 
 
-const customerDetails = new CustomerDetails();
-customerDetails.printFirstName('Holly');
-customerDetails.printFirstName('Hobby');
+// Importing functions and class
+import { add, subtract } from './';
+import Person from './person.js';
 
+// Using the imported functions
+console.log(add(2, 3)); // Output: 5
+console.log(subtract(5, 2)); // Output: 3
+
+// Using the imported class
+const john = new Person('John', 25);
+john.greet(); // Output: Hello, my name is John and I am 25 years old.
 
  //instant of the class has been created here, var CustomerDetails is an object
