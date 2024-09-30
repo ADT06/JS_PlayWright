c1c22da3208d8b2e7d84f43b1a18a24ee2a385a6
ADT06 <anu.tewari@ishidaeurope.com>
2024-09-30 09:39:43 +0100

    lesson9 class and methods

--- helpers/printHelper.js
+++ helpers/printHelper.js
@@ -11,3 +11,12 @@ printAge()
 //And then you simply call this function and use it.
 // Also, you can import all the functions from the JavaScript using import start as provide the name of the object, 
 //how you want to use this function, and then through the dot notation you call the function inside of the file.
+
+//create inside the the class the methods
+export class CustomerDetails {
+    printFirstName(name) {
+        console.log(name);
+    }
+}
+ //class gets created by the keyword and use capitals in the beginning of the word
+ //if you export the class it will be visible in other files in JS 
---
+++ lesson9.js
@@ -0,0 +1,10 @@
+//Class and Methods
+
+//type import and name and vscode will give a hint which class is available to choose and by clicking enter, vs code will create the import
+import {CustomerDetails} from "./JS_PlayWright/helpers/printHelper.js"; 
+
+const customerDetails = new CustomerDetails();
+customerDetails.printFirstName('Holly');
+customerDetails.printFirstName('Hobby');
+
+ //instant of the class has been created here, var CustomerDetails is an object
