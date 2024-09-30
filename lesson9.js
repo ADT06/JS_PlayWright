8600e06dbf4cf75a09c5ee2f5a224dcbaacae3f3
ADT06 <anu.tewari@ishidaeurope.com>
2024-09-30 09:46:35 +0100

    save changes

--- lesson9.js
+++ lesson9.js
@@ -1,10 +1,11 @@
 //Class and Methods
 
 //type import and name and vscode will give a hint which class is available to choose and by clicking enter, vs code will create the import
-import {CustomerDetails} from "./JS_PlayWright/helpers/printHelper.js"; 
+import {CustomerDetails} from "./"; 
 
 const customerDetails = new CustomerDetails();
 customerDetails.printFirstName('Holly');
 customerDetails.printFirstName('Hobby');
+
 
  //instant of the class has been created here, var CustomerDetails is an object
